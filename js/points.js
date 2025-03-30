84fe6ce7fa9b114dad5f64c4507c26a5087c4ec4
kvloginov <kostghost@gmail.com>
2025-03-30 21:26:56 +0500

    feat: implement custom marker styles and update icon creation for points

--- js/points.js
+++ js/points.js
@@ -216,22 +216,20 @@ const Points = {
         });
 
         // Create marker icons
-        const defaultIcon = L.icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        const defaultIcon = L.divIcon({
+            className: 'custom-marker',
+            html: '<div class="marker-inner"></div>',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
+            popupAnchor: [1, -34]
         });
 
-        const selectedIcon = L.icon({
-            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        const selectedIcon = L.divIcon({
+            className: 'custom-marker selected',
+            html: '<div class="marker-inner"></div>',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
+            popupAnchor: [1, -34]
         });
 
         // Redraw all points
